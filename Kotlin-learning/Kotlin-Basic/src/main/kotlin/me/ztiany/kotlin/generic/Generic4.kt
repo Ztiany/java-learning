1fc6cd53526e9bb89149e44ed6eed11cf75e6b40
Alien <ztiany3@gmail.com>
2021-09-28 18:48:09 +0800

    update kotlin

---
+++ Kotlin-learning/Kotlin-Basic/src/main/kotlin/me/ztiany/kotlin/generic/Generic4.kt
@@ -0,0 +1,20 @@
+package me.ztiany.kotlin.generic
+
+
+private interface ProducerA<out T>{
+
+    fun getData():T
+
+}
+private interface ProducerB<out T:Any>{
+    fun getData():T
+
+}
+
+
+private fun testNullGeneric() {
+    var producerA:ProducerA<String>
+    //var producerB:ProducerB<String?>
+    //val dataA = producerA.getData()
+    //val dataB = producerB.getData()
+}
