40ba5a8719c752654710da516746d42ed0ec5ca3
Alien <Ztiany@users.noreply.github.com>
2021-10-12 18:16:53 +0800

    update kotlin

--- Kotlin-learning/Kotlin-Basic/src/main/04-coroutines/core/exception_supervision/02_SupervisionCope.kt
+++ Kotlin-learning/Kotlin-Basic/src/main/04-coroutines/core/exception_supervision/02_SupervisionCope.kt
@@ -5,8 +5,8 @@ import kotlinx.coroutines.*
 
 /*
 对于作用域的并发，supervisorScope 可以被用来替代 coroutineScope 来实现相同的目的。它只会单向的传播并且当子任务自身执行失败的时候将它们全部取消。
-它也会在所有的子任务执行结束前等待， 就像 coroutineScope 所做的那样。
- */
+它也会在所有的子任务执行结束前等待，就像 coroutineScope 所做的那样。
+*/
 fun main() = runBlocking {
 
     try {
@@ -26,10 +26,10 @@ fun main() = runBlocking {
             println("Throwing exception from scope")
             throw AssertionError()
         }
+
     } catch (e: AssertionError) {
         println("Caught assertion error")
     }
 
     println("end...")
-
 }
