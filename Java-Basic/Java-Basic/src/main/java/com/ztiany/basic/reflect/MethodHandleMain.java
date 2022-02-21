d4db37894f2a84a57ef2253dfaec9aab4c9e8f0d
ztiany <ztiany3@gmail.com>
2022-02-22 01:04:22 +0800

    update jvm

---
+++ Java-Basic/Java-Basic/src/main/java/com/ztiany/basic/reflect/MethodHandleMain.java
@@ -0,0 +1,22 @@
+package com.ztiany.basic.reflect;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+public class MethodHandleMain {
+
+    public void print(String s) {
+        System.out.println("hello, " + s);
+    }
+
+
+    public static void main(String[] args) throws Throwable {
+        MethodHandleMain foo = new MethodHandleMain();
+
+        MethodType methodType = MethodType.methodType(void.class, String.class);
+        MethodHandle methodHandle = MethodHandles.lookup().findVirtual(MethodHandleMain.class, "print", methodType);
+        methodHandle.invokeExact(foo, "world");
+    }
+
+}
